b8156a3eaad36d6456ec901d5dc2610ca728dfce
sza-wtag <samia.zahan@welldev.io>
2023-06-26 14:00:54 +0600

    icons are imported as component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { DEFAULT_IMAGE_TEXT } from 'src/constants/constant';
+import { ICONS } from 'src/assets/icons/icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
   imgText = DEFAULT_IMAGE_TEXT;
-  logoIconUrl = "../../../assets/icons/logo.svg";
-  searchIconUrl = "../../../assets/icons/searchIcon.svg";
+  logoIconUrl =ICONS.LOGO;
+  searchIconUrl = ICONS.SEARCH;
 }
