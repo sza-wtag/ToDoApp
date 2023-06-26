b5efe5f12e1ea36939e735fb540d1811b456b5d2
sza-wtag <samia.zahan@welldev.io>
2023-06-26 15:47:40 +0600

    spacing and tag fixed

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy , ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility.service';
-import {DEFAULT_IMAGE_TEXT} from "src/constants/constant";
+import { DEFAULT_IMAGE_TEXT } from "src/constants/constant";
 import { ICONS } from 'src/assets/icons/icons';
 
 
@@ -10,7 +10,7 @@ import { ICONS } from 'src/assets/icons/icons';
   styleUrls: ['./splash-screen.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class SplashScreenComponent implements OnInit , OnDestroy {
+export class SplashScreenComponent implements OnInit, OnDestroy {
   showSplash: boolean;
   readonly imgText= DEFAULT_IMAGE_TEXT;
   timeoutID: number;
