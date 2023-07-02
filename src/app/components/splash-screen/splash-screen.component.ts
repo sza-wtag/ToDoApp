80b0c5ed3e09a50ed6e751f400d303d5384ce82b
sza-wtag <samia.zahan@welldev.io>
2023-07-02 10:44:54 +0600

    showSplash state managed locally

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { UtilityService } from 'src/app/services/utility.service';
 import { DEFAULT_IMAGE_TEXT } from "src/constants/constant";
 import { ICONS } from 'src/assets/icons/icons';
 
@@ -13,12 +12,9 @@ export class SplashScreenComponent implements OnInit, OnDestroy {
   readonly iconImageUrl = ICONS.LOGO;
   readonly imgText= DEFAULT_IMAGE_TEXT;
   private timeoutID: ReturnType<typeof setTimeout>;
-  showSplash: boolean;
-
-  constructor(private utilityService: UtilityService) {}
+  protected showSplash = true;
 
   ngOnInit() {
-    this.showSplash = this.utilityService.showSplash;
     this.timeoutID = setTimeout(() => {
       this.showSplash = false
     }, 2000);
