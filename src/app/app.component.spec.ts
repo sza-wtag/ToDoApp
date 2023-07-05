bf7d7e12f88673ac9c0d0ce03825fae5da3407d5
sza-wtag <samia.zahan@welldev.io>
2023-07-05 16:03:44 +0600

    Test is writen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent, SplashScreenComponent, HeaderComponent]
   }));
 
   it('should create the app', () => {
@@ -18,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ToDoApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ToDoApp app is running!');
   });
 });
--- src/app/components/splash-screen/splash-screen.component.spec.ts
+++ src/app/components/splash-screen/splash-screen.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SplashScreenComponent } from './splash-screen.component';
+import { By } from '@angular/platform-browser';
 
 describe('SplashScreenComponent', () => {
   let component: SplashScreenComponent;
@@ -17,5 +18,19 @@ describe('SplashScreenComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display app-splash-screen component if showSplash is true', () => {
+    component.setShowSplash(true)
+    fixture.detectChanges();
+    const addSplashScreen = fixture.debugElement.query(By.css('.app-splash-screen'));
+    expect(addSplashScreen).toBeTruthy();
+  });
+
+  it('should not display app-splash-screen component if showSplash is false', () => {
+    component.setShowSplash(false)
+    fixture.detectChanges();
+    const addSplashScreen = fixture.debugElement.query(By.css('.app-splash-screen'));
+    expect(addSplashScreen).toBeFalsy();
   });
 });
